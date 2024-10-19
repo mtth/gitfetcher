fa8235f52a5637a396c387e2f123bd562d0f8f5d
Matthieu Monsch <matthieu.monsch@gmail.com>
2024-10-19 14:35:58 -0700

    fixup! test: enable source fetcher tests

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+shopt -s nullglob
+
+usage() { # [CODE]
+	local cmd="${0##*/}"
+	cat <<-EOF
+		Install package dependencies
+
+		Usage:
+		  $cmd [-h]
+
+		Options:
+		  -h  Show this message and exit.
+	EOF
+	exit "${1:-2}"
+}
+
+main() {
+	local opt
+	while getopts :h opt "$@"; do
+		case "$opt" in
+			h) usage 0 ;;
+			*) echo "unknown option: $OPTARG" >&2 && usage ;;
+		esac
+	done
+	shift $(( OPTIND-1 ))
+
+	go mod download
+	go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.35.1
+	go generate ./...
+}
+
+main "$@"
