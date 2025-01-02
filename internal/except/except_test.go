4a5f07dacfe9a8241b16033ba5a1256e1f06211e
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-01-02 09:59:02 +0100

    fixup! feat: add local target detection

--- internal/except/except_test.go
+++ internal/except/except_test.go
@@ -1,8 +1,10 @@
 package except
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,5 +17,17 @@ func TestMust(t *testing.T) {
 		require.Panics(t, func() {
 			Must(false, "panic")
 		})
+	})
+}
+
+func TestLogErrAttr(t *testing.T) {
+	t.Run("nil error", func(t *testing.T) {
+		got := LogErrAttr(nil)
+		assert.Equal(t, "[]", got.Value.String())
+	})
+
+	t.Run("no-nil error", func(t *testing.T) {
+		got := LogErrAttr(errors.New("boom"))
+		assert.Contains(t, got.Value.String(), "boom")
 	})
 }
