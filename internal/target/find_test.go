2b8c40ddbfb08572544a21d0d325d0bd033c1e09
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-01-03 08:24:14 +0100

    fixup! feat: add local target detection

--- internal/target/find_test.go
+++ internal/target/find_test.go
@@ -1,19 +1,14 @@
 package target
 
 import (
-	"io/fs"
 	"path/filepath"
 	"testing"
 	"testing/fstest"
-	"time"
 
-	"github.com/mtth/gitfetcher/internal/effect"
 	"github.com/mtth/gitfetcher/internal/fspath"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var emptyFile = &fstest.MapFile{}
 
 func bare(dpath fspath.Local) Target {
 	return realTarget{gitDir: dpath}
@@ -74,26 +69,4 @@ func TestFind(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, []Target{bare("/root/one.git"), bare("/root/two.git")}, targets)
 	})
-}
-
-func TestRemoteRefUpdateTimes(t *testing.T) {
-	t1 := time.UnixMilli(time.Hour.Milliseconds())
-	t2 := time.UnixMilli(2 * time.Hour.Milliseconds())
-	t3 := time.UnixMilli(3 * time.Hour.Milliseconds())
-	t.Run("simple", func(t *testing.T) {
-		defer swapFileSystem(fstest.MapFS{
-			"root/one.git/HEAD":                        emptyFile,
-			"root/one.git/objects":                     emptyFile,
-			"root/one.git/refs/remotes/origin/main":    &fstest.MapFile{ModTime: t1},
-			"root/one.git/refs/remotes/origin/foo/one": &fstest.MapFile{ModTime: t2},
-			"root/one.git/refs/remotes/origin/foo/two": &fstest.MapFile{ModTime: t3},
-			"root/one.git/refs/remotes/other/bar":      &fstest.MapFile{ModTime: t2},
-		})()
-		times := remoteRefUpdateTimes("/root/one.git")
-		assert.Equal(t, map[string]time.Time{"main": t1, "foo/one": t2, "foo/two": t3}, times)
-	})
-}
-
-func swapFileSystem(mapfs fstest.MapFS) func() {
-	return effect.Swap[fs.FS](&fileSystem, mapfs)
 }
--- internal/target/target_test.go
+++ internal/target/target_test.go
@@ -1,8 +1,12 @@
 package target
 
 import (
+	"io/fs"
 	"testing"
+	"testing/fstest"
+	"time"
 
+	"github.com/mtth/gitfetcher/internal/effect"
 	"github.com/mtth/gitfetcher/internal/fspath"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -26,4 +30,36 @@ func TestFromPath(t *testing.T) {
 		assert.NotNil(t, got)
 		assert.False(t, IsBare(got))
 	})
+}
+
+func TestTarget_RemoteLastUpdatedAt(t *testing.T) {
+	t1 := time.UnixMilli(time.Hour.Milliseconds())
+	t2 := time.UnixMilli(2 * time.Hour.Milliseconds())
+	t3 := time.UnixMilli(3 * time.Hour.Milliseconds())
+
+	defer swapFileSystem(fstest.MapFS{
+		"root/one.git/HEAD":                        emptyFile,
+		"root/one.git/objects":                     emptyFile,
+		"root/one.git/refs/remotes/origin/main":    &fstest.MapFile{ModTime: t1},
+		"root/one.git/refs/remotes/origin/foo/one": &fstest.MapFile{ModTime: t2},
+		"root/one.git/refs/remotes/origin/foo/two": &fstest.MapFile{ModTime: t1},
+		"root/one.git/refs/remotes/other/bar":      &fstest.MapFile{ModTime: t3},
+	})()
+
+	t.Run("underlying times ", func(t *testing.T) {
+		times := remoteRefUpdateTimes("/root/one.git")
+		assert.Equal(t, map[string]time.Time{"main": t1, "foo/one": t2, "foo/two": t1}, times)
+	})
+
+	t.Run("method", func(t *testing.T) {
+		tgt, err := FromPath("/root/one.git")
+		require.NoError(t, err)
+		assert.Equal(t, t2, tgt.RemoteLastUpdatedAt())
+	})
+}
+
+var emptyFile = &fstest.MapFile{}
+
+func swapFileSystem(mapfs fstest.MapFS) func() {
+	return effect.Swap[fs.FS](&fileSystem, mapfs)
 }
